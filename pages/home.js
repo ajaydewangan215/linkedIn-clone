d3f80d1865319db40f23d44f8f34a930ed8a61c9
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-07-28 16:25:02 +0530

    updated

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+const home = () => {
+  return (
+    <div className="space-y-10">
+      {/* <Head>
+        <title>LindIn Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> */}
+      Home
+    </div>
+  );
+};
+
+export default home;
