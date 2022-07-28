7f991fc94f6c77e5007ccd50d5d07637894cdb71
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-07-28 14:06:09 +0530

    add home page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  darkMode:"class",
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['content.linkedin.com', 'picsum.photos', 'static-exp1.licdn.com', 'www.iconsdb.com', 'images.fastcompany.net']
+  },
 }
 
 module.exports = nextConfig
+
